7018537e9003a1fd311a5875d12f135b9057c184
Arnaud Fontaine <arnau@debian.org>
2012-02-10 10:44:51 +0900

    Add copyright statement and update accordingly COPYING file.
    Reviewed-by: Gaetan Nadon<memsize@videotron.ca>

--- transSet.c
+++ transSet.c
@@ -1,15 +1,29 @@
-/* Simple program to toggle Translucency property
-   Based on 'transset' by  Matthew Hawn
-   With some additional features to make it more automatic and integrated
-   The purpose is to bind it from your wm to a key or mouse-button
-
-   License: Use however you want.
-   Authors:
-   Matthew Hawn
-   Daniel Forchheimer
-   Andreas Kohn
-   Roman Divacky
-*/
+/* Copyright © 2003-2004 Matthew Hawn
+ * Copyright © 2003-2004 Andreas Kohn
+ * Copyright © 2003-2004 Roman Divacky
+ * Copyright © 2003-2004 Keith Packard
+ * Copyright © 2005-2007 Daniel Forchheimer
+ * Copyright © 2011-2012 Arnaud Fontaine
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
 
 #define VERSIONSTR "6"
 #define RELEASEDATESTR "2007-09-21"
